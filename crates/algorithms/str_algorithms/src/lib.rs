3b3c173851348b8b086e5a7ae4535fdd2503476f
Whitedwarf137 <whitedwarf137@gmail.com>
2020-10-01 15:56:12 +0900

    renamed to 'str_algorithms, added KMP algorithm

--- crates/algorithms/str_algorithms/src/lib.rs
+++ crates/algorithms/str_algorithms/src/lib.rs
@@ -19,6 +19,46 @@ pub fn z_algorithm<T: Ord>(s: &[T]) -> Vec<usize> {
     ret
 }
 
+pub fn kmp_table<T: Ord>(pattern: &[T]) -> Vec<usize> {
+    let n = pattern.len();
+    let mut ret = vec![0; n];
+    let mut j = 0;
+    for i in 1..n {
+        ret[i] = j;
+        if pattern[i] == pattern[j] {
+            j += 1;
+        } else {
+            j = 0;
+        }
+    }
+    ret
+}
+
+pub fn kmp_search<T: Ord>(pattern: &[T], target: &[T]) -> Option<usize> {
+    let n = pattern.len();
+    let m = target.len();
+    let table = kmp_table(&pattern);
+    let mut i = 0;
+    let mut p = 0;
+    while i < m && p < n {
+        if target[i] == pattern[p] {
+            i += 1;
+            p += 1;
+        } else if p == 0 {
+            i += 1;
+        } else {
+            p = table[p];
+        }
+    }
+    if p == n {
+        Some(i - p)
+    } else {
+        None
+    }
+}
+
+
+
 #[cfg(test)]
 mod tests {
     use super::z_algorithm;
@@ -38,12 +78,12 @@ mod tests {
     #[test_case("abcabca")]
     #[test_case("abracadabra")]
     #[test_case("mississippi")]
-    fn test_hand(s: &str) {
+    fn test_z_hand(s: &str) {
         assert_eq!(z_brute(s.as_bytes()), z_algorithm(s.as_bytes()));
     }
 
     #[test]
-    fn test_random() {
+    fn test_z_random() {
         let mut rng = StdRng::seed_from_u64(137);
         for _ in 0..20 {
             let n = rng.gen_range(30, 100);
