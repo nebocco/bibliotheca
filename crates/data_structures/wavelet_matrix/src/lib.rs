e7da4ca15458eb7c9e288256feb6b1438f589e74
Whitedwarf137 <whitedwarf137@gmail.com>
2020-10-02 17:23:27 +0900

    start creating Wavelet Matrix

---
+++ crates/data_structures/wavelet_matrix/src/lib.rs
@@ -0,0 +1,17 @@
+use bit_vector::BitVector;
+
+struct WaveletMatrix();
+
+impl WaveletMatrix {
+    fn new() -> Self {
+        WaveletMatrix{}
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
