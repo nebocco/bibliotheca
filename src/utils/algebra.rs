2d83ed9ea6fff27e1ee08648186d781b51097ec9
Whitedwarf137 <whitedwarf137@gmail.com>
2020-10-07 10:29:30 +0900

    added commutative structures

--- src/utils/algebra.rs
+++ src/utils/algebra.rs
@@ -43,13 +43,22 @@ impl<T: Magma + Associative> SemiGroup for T {}
 trait Monoid: SemiGroup + Unital {}
 impl<T: SemiGroup + Unital> Monoid for T {}
 
+trait ComMonoid: Monoid + Commutative {}
+impl<T: Monoid + Commutative> ComMonoid for T {}
+
 /// 群
 trait Group: Monoid + Invertible {}
 impl<T: Monoid + Invertible> Group for T {}
 
+trait ComGroup: Group + Commutative {}
+impl<T: Group + Commutative> ComGroup for T {}
+
 /// 環
 trait Ring: Element + Zero + One + Sub<Output=Self> + SubAssign + Neg {}
 impl<T: Element + Zero + One + Sub<Output=Self> + SubAssign + Neg> Ring for T {}
+
+trait ComRing: Ring + Commutative {}
+impl<T: Ring + Commutative> ComRing for T {}
 
 /// 体
 trait Field: Ring + Div<Output=Self> + DivAssign {}
