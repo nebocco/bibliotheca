b6ace07baae3c544d084818ab5312a49748bb2d7
Whitedwarf137 <whitedwarf137@gmail.com>
2020-10-07 11:30:17 +0900

    make new one

--- src/utils/argebraic_new.rs
+++ src/utils/argebraic_new.rs
@@ -1,13 +1,7 @@
-#[allow(unused_imports)]
+#![allow(unused_imports)]
+use std::marker::Sized;
+use num_traits::{One, Zero};
 use std::ops::{Add, Sub, Mul, Div, Neg, AddAssign, SubAssign, MulAssign, DivAssign};
-
-/// 元
-trait Element: Sized + Clone + PartialEq {}
-
-/// 二項演算
-trait Magma: Element {
-    fn op(&self, rhs: &Self) -> Self;
-}
 
 /// 結合性
 trait Associative: Magma {}
@@ -15,43 +9,39 @@ trait Associative: Magma {}
 /// 可換性
 trait Commutative: Magma {}
 
-/// 単元
-trait Unital: Magma {
-    fn identity() -> Self;
-}
-
 /// 可逆性
 trait Invertible: Magma {
     fn inverse(&self) -> Self;
 }
 
-/// 加法単元
-trait Zero: Element + Add<Output=Self> + AddAssign {
-    fn zero() -> Self;
-}
+/// 元
+trait Element: Sized + Clone + PartialEq {}
+impl<T: Sized + Clone + PartialEq> Element for T {}
 
-/// 乗法単元
-trait One: Element + Mul<Output=Self> + MulAssign {
-    fn one() -> Self;
-}
+/// マグマ
+trait Magma: Element + Add<Output = Self> + AddAssign {}
+impl<T: Element + Add + AddAssign> Magma for T {}
 
 /// 半群
 trait SemiGroup: Magma + Associative {}
 impl<T: Magma + Associative> SemiGroup for T {}
 
 /// モノイド
-trait Monoid: SemiGroup + Unital {}
-impl<T: SemiGroup + Unital> Monoid for T {}
+trait Monoid: SemiGroup + Zero {}
+impl<T: SemiGroup + Zero> Monoid for T {}
 
 trait ComMonoid: Monoid + Commutative {}
 impl<T: Monoid + Commutative> ComMonoid for T {}
 
 /// 群
-trait Group: Monoid + Invertible {}
-impl<T: Monoid + Invertible> Group for T {}
+trait Group: Monoid + Sub<Output=Self> + SubAssign + Neg<Output=Self> {}
+impl<T: Monoid + Sub + SubAssign + Neg> Group for T {}
 
 trait ComGroup: Group + Commutative {}
 impl<T: Group + Commutative> ComGroup for T {}
+
+/// 半環
+trait SemiRing
 
 /// 環
 trait Ring: Element + Zero + One + Sub<Output=Self> + SubAssign + Neg {}
