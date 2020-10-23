f6084c0775c8c168146dac2889482f961adca2df
Whitedwarf137 <whitedwarf137@gmail.com>
2020-10-23 17:04:46 +0900

    scratch dinic

--- src/algorithms.rs
+++ src/algorithms.rs
@@ -1,5 +1,6 @@
 pub mod baby_step_giant_step;
 pub mod dijkstra;
+pub mod dinic;
 pub mod dp;
 pub mod scc;
 pub mod str_algorithms;
---
+++ src/algorithms/dinic.rs
@@ -0,0 +1,34 @@
+#![allow(dead_code)]
+use num_traits::{Num, Unsigned};
+
+pub trait FlowTrait: Num + Unsigned {}
+impl<T: Num + Unsigned> FlowTrait for T {}
+
+#[derive(Debug, Clone)]
+struct FlowEdge<Value: FlowTrait> {
+	from: usize,
+	to: usize,
+	cap: Value
+}
+
+#[derive(Debug, Clone)]
+struct DoubleEdge {
+    from: usize,
+    from_idx: usize,
+    to: usize,
+    to_idx: usize,
+}
+
+/// Dinic 法で最大流を求めます。
+pub struct MaxFlow<Value: FlowTrait> {
+    /// 最大流のアルゴリズムを適用済みかどうかです。
+    source: usize,
+    sink: usize,
+    network: Vec<Vec<FlowEdge<Value>>>,
+}
+
+impl<Value: FlowTrait> MaxFlow<Value> {
+    fn len(&self) -> usize {
+        self.network.len()
+	}
+}
