c93dc585e108834706d7ff2667e91f1a5b7421cd
Whitedwarf137 <whitedwarf137@gmail.com>
2020-10-29 12:04:51 +0900

    separated into folders

---
+++ src/data_structures/segment_tree/segment_tree.rs
@@ -0,0 +1,109 @@
+#![allow(dead_code)]
+use std::ops::Range;
+use crate::utils::algebraic_traits::{/* Element, */ Monoid};
+
+pub struct SegmentTree<T: Monoid> {
+	size: usize,
+	node: Vec<T>
+}
+
+impl<T: Monoid> SegmentTree<T> {
+	fn new(n0: usize) -> Self {
+		let size = n0.next_power_of_two();
+		let node = vec![T::zero(); size * 2];
+		SegmentTree {
+			size, node
+		}
+	}
+
+	fn from(vec: &[T]) -> Self {
+		let size = vec.len().next_power_of_two();
+		let mut node = vec![T::zero(); size << 1];
+		for i in 0..vec.len() {
+			node[i + size] = vec[i].clone();
+		}
+		for i in (1..size).rev() {
+			node[i] = node[i << 1].clone() + node[(i << 1) + 1].clone();
+		}
+		SegmentTree {
+			size, node
+		}
+	}
+
+	pub fn get(&self, i: usize) -> &T { &self.node[i + self.size] }
+
+	pub fn set(&mut self, mut i: usize, x: T) {
+		i += self.size;
+		self.node[i] = x;
+		self.fix(i);
+	}
+
+	fn fix(&mut self, mut i: usize) {
+        while i > 0 {
+			i >>= 1;
+			self.node[i] = self.node[i << 1].clone() + self.node[(i << 1) + 1].clone();
+		}
+    }
+
+	pub fn fold(&self, rng: Range<usize>) -> T {
+		let mut vl = T::zero();
+		let mut vr = T::zero();
+		let mut l = rng.start + self.size;
+		let mut r = rng.end + self.size;
+		while l < r {
+			if l & 1 == 1 {
+				vl = vl + self.node[l].clone();
+				l += 1;
+			}
+			if r & 1 == 1 {
+				r -= 1;
+				vr = self.node[r].clone() + vr;
+			}
+			l >>= 1;
+			r >>= 1;
+		}
+		vl + vr
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	use crate::utils::algebraic_traits::*;
+	use std::ops::Add;
+	use num_traits::Zero;
+
+	#[derive(Clone, PartialEq)]
+    struct Am(usize);
+
+    impl Add for Am {
+		type Output = Self;
+        fn add(self, right: Self) -> Self { Am(self.0 + right.0) }
+	}
+
+    impl Associative for Am {}
+
+    impl Zero for Am {
+		fn zero() -> Self { Am(0) }
+		fn is_zero(&self) -> bool { self.0 == 0 }
+	}
+
+    #[test]
+    fn rsq_test() {
+        let mut seg = SegmentTree::from(&vec![Am(1), Am(2), Am(3)]);
+        assert!(seg.fold(0..2).0 == 3);
+        assert!(seg.fold(1..2).0 == 2);
+        seg.set(1, Am(5));
+        assert!(seg.fold(0..2).0 == 6);
+        assert!(seg.fold(1..2).0 == 5);
+        seg.set(1, seg.get(1).clone() + Am(5));
+        assert!(seg.fold(0..2).0 == 11);
+        assert!(seg.fold(1..2).0 == 10);
+	}
+
+	#[test]
+    fn corner_test() {
+        let seg = SegmentTree::from(&vec![Am(1)]);
+        assert!(seg.fold(0..1).0 == 1);
+    }
+}
---
+++ src/data_structures/union_find/union_find.rs
@@ -0,0 +1,125 @@
+/*
+
+#[derive(Clone, Copy, Debug)]
+enum PatentOrSize {
+    Parent(usize),
+    Size(usize),
+}
+
+#[derive(Clone, Debug)]
+pub struct UnionFind(Vec<PatentOrSize>);
+
+impl UnionFind {
+    pub fn new(len: usize) -> Self {
+        Self(vec![PatentOrSize::Size(1); len])
+    }
+
+    pub fn find(&mut self, i: usize) -> usize {
+        self._climb(i).0
+    }
+
+    pub fn size(&mut self, i: usize) -> usize {
+        self._climb(i).1
+    }
+
+    pub fn unite(&mut self, u: usize, v: usize) -> Result<(), ()> {
+        let (mut u, su) = self._climb(u);
+        let (mut v, sv) = self._climb(v);
+        if u == v { return Err(()); }
+        if su < sv {
+            std::mem::swap(&mut u, &mut v);
+        }
+        self.0[u] = PatentOrSize::Size(su + sv);
+        self.0[v] = PatentOrSize::Parent(u);
+        Ok(())
+    }
+
+    pub fn is_same(&mut self, u: usize, v:usize) -> bool {
+        self.find(u) == self.find(v)
+    }
+
+    fn _climb(&mut self, i: usize) -> (usize, usize) {
+        assert!(i < self.0.len());
+        match self.0[i] {
+            PatentOrSize::Parent(p) => {
+                let ret = self._climb(p);
+                self.0[i] = PatentOrSize::Parent(ret.0);
+                ret
+            }
+            PatentOrSize::Size(s) => (i, s)
+        }
+    }
+}
+
+*/
+
+#[derive(Clone, Debug)]
+pub struct UnionFind(Vec<isize>);
+
+impl UnionFind {
+    pub fn new(len: usize) -> Self {
+        Self(vec![-1; len])
+    }
+
+    pub fn find(&mut self, i: usize) -> usize {
+        self._climb(i).0
+    }
+
+    pub fn size(&mut self, i: usize) -> usize {
+        self._climb(i).1
+    }
+
+    pub fn unite(&mut self, u: usize, v: usize) -> Result<(), ()> {
+        let (mut u, su) = self._climb(u);
+        let (mut v, sv) = self._climb(v);
+        if u == v { return Err(()); }
+        if su < sv {
+            std::mem::swap(&mut u, &mut v);
+        }
+        self.0[u] += self.0[v];
+        self.0[v] = u as isize;
+        Ok(())
+    }
+
+    pub fn is_same(&mut self, u: usize, v:usize) -> bool {
+        self.find(u) == self.find(v)
+    }
+
+    fn _climb(&mut self, i: usize) -> (usize, usize) {
+        assert!(i < self.0.len());
+        let mut v = i;
+        while self.0[v] >= 0 {
+            let p = self.0[v] as usize;
+            if self.0[p] >= 0 {
+                self.0[v] = self.0[p];
+                v = self.0[p] as usize;
+            } else {
+                v = p;
+            }
+        }
+        (v, -self.0[v] as usize)
+    }
+
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::UnionFind;
+    // * verified at https://judge.yosupo.jp/submission/26460
+    #[test]
+    fn tset_union_find() {
+        let mut uf = UnionFind::new(6);
+        assert_eq!(uf.size(2), 1);
+        assert_eq!(uf.find(5), 5);
+        uf.unite(0, 4).ok();
+        uf.unite(2, 3).ok();
+        uf.unite(1, 4).ok();
+        uf.unite(1, 0).ok();
+        assert_eq!(uf.find(5), 5);
+        assert_eq!(uf.find(2), uf.find(3));
+        assert_eq!(uf.size(0), 3);
+        assert_ne!(uf.find(5), uf.find(0));
+        assert!(uf.is_same(1, 4));
+    }
+}
