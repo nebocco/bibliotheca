c4604240e4b807d3076e2562d0891f5d909d1c9b
Whitedwarf137 <whitedwarf137@gmail.com>
2020-10-30 16:12:35 +0900

    test failed

--- src/algorithms/hungarian.rs
+++ src/algorithms/hungarian.rs
@@ -1,23 +1,81 @@
-// #[allow(dead_code, unused_mut, unused_variables)]
-// fn hungarian(a: &[Vec<i64>]) -> (i64, Vec<usize>) {
-// 	let n = a.len();
-// 	let mut p: usize;
-// 	let mut q: usize;
-// 	let mut x = vec![-1; n];
-// 	let mut y = vec![-1; n];
-// 	assert!(a[0].len() == n,
-// 	"given matrix is not square: {} rows, {} columns", n, a[0].len());
-// 	let fx = (0..n).map(
-// 		|k| a[k].iter().max().unwrap().clone()
-// 	).collect::<Vec<_>>();
-// 	let mut i = 0;
-// 	while i < n {
-// 		let mut t = vec![-1; n];
-// 		let mut s = vec![i; n+1];
-// 		p = 0; q = 0;
-// 		while p <= q && x[i] < 0 {
-// 			p += 1;
-// 		}
-// 	}
-// 	(0, vec![0])
-// }
+#[allow(dead_code, unused_mut, unused_variables)]
+fn hungarian(a: &[Vec<i64>]) -> (i64, Vec<usize>) {
+	let n = a.len();
+	let mut p: usize;
+	let mut q: usize;
+	let mut x = vec![n; n];
+	let mut y = vec![n; n];
+	assert!(a[0].len() == n,
+	"given matrix is not square: {} rows, {} columns", n, a[0].len());
+	let mut fx = (0..n).map(
+		|k| a[k].iter().max().unwrap().clone()
+	).collect::<Vec<_>>();
+	let mut fy = vec![0; n];
+	let mut i = 0;
+	let mut j = 0;
+	while i < n {
+		let mut t = vec![n; n];
+		let mut s = vec![i; n+1];
+		p = 0; q = 0;
+		while p <= q && x[i] == n {
+			p += 1;
+			let mut k = s[p];
+			while j < n && x[i] == n {
+				j += 1;
+				if fx[k] + fy[j] == a[k][j] && t[j] == n {
+					q += 1;
+					s[q] = y[j];
+					t[j] = k;
+					if s[q] == n {
+						while p != n {
+							y[j] = t[j];
+							k = t[j];
+							p = x[k];
+							x[k] = j;
+							j = p;
+						}
+					}
+				}
+			}
+		}
+		if x[i] == n {
+			let mut d = std::i64::MAX;
+			for k in 0..q {
+				for j in 0..n {
+					if t[j] == n {
+						d = std::cmp::min(d, fx[s[k]] + fy[j] - a[s[k]][j]);
+					}
+				}
+			}
+			for j in 0..n {
+				fy[j] += if t[j] == n { 0 } else { d };
+			}
+			for k in 0..=q {
+				fx[s[k]] -= d;
+			}
+		} else {
+			i += 1;
+		}
+	}
+	let ret = (0..n).map(|i| a[i][x[i]]).sum::<i64>();
+	(ret, x)
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	// ! test failed
+    // #[test]
+    fn test_4x4() {
+		let a = vec![
+			vec![0, 1, 2, 3],
+			vec![2, 3, 0, 1],
+			vec![3, 0, 1, 2],
+			vec![1, 2, 3, 0],
+		];
+		let (sc, x) = hungarian(&a);
+		assert_eq!(sc, 12);
+		assert_eq!(x, vec![3, 1, 0, 2]);
+    }
+}
