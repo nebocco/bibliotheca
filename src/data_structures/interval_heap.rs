5376ed75c573fdc10de293a75950d0dcc26d84ee
Whitedwarf137 <whitedwarf137@gmail.com>
2020-10-30 10:56:31 +0900

    test passed

--- src/data_structures/interval_heap.rs
+++ src/data_structures/interval_heap.rs
@@ -23,12 +23,10 @@ impl<T: Element + Ord> DoublePriorityHeap<T> {
 		if self.0.len() < 3 {
 			self.0.pop()
 		} else {
-			let n = self.0.len();
-			self.0.swap(1, n - 1);
-			let ret = self.0.pop();
+			let ret = self.0.swap_remove(1);
 			let k = self.down(1);
 			self.up(k, 1);
-			ret
+			Some(ret)
 		}
 	}
 
@@ -36,12 +34,10 @@ impl<T: Element + Ord> DoublePriorityHeap<T> {
 		if self.0.len() < 2 {
 			self.0.pop()
 		} else {
-			let n = self.0.len();
-			self.0.swap(1, n - 1);
-			let ret = self.0.pop();
+			let ret = self.0.swap_remove(0);
 			let k = self.down(0);
 			self.up(k, 1);
-			ret
+			Some(ret)
 		}
 	}
 
@@ -70,19 +66,33 @@ impl<T: Element + Ord> DoublePriorityHeap<T> {
 
 	#[inline]
 	fn parent(k: usize) -> usize {
-		(k >> 1).wrapping_sub(1) & 1usize.wrapping_neg()
+		(k >> 1).wrapping_sub(1) & !1
 	}
 
 	fn down(&mut self, mut k: usize) -> usize {
 		let n = self.0.len();
-		if k & 1 == 1 {
+		let mut c: usize;
+		if k & 1 == 1 { // min heap
 			while 2 * k + 1 < n {
-				let mut c = 2 * k + 4;
+				c = 2 * k + 3;
 				if n <= c || self.0[c-2] < self.0[c] {
 					c -= 2;
 				}
 				if c < n && self.0[c] < self.0[k] {
-					self.0.swap(c, k);
+					self.0.swap(k, c);
+					k = c;
+				} else {
+					break
+				}
+			}
+		} else { // max heap
+			while 2 * k + 2 < n {
+				c = 2 * k + 4;
+				if n <= c || self.0[c] < self.0[c-2] {
+					c -= 2;
+				}
+				if c < n && self.0[k] < self.0[c] {
+					self.0.swap(k, c);
 					k = c;
 				} else {
 					break
@@ -93,21 +103,65 @@ impl<T: Element + Ord> DoublePriorityHeap<T> {
 	}
 
 	fn up(&mut self, mut k: usize, root: usize) {
-		if (k | 1) < self.0.len() && self.0[k & 1usize.wrapping_neg()] < self.0[k | 1] {
-			self.0.swap(k & 1usize.wrapping_neg(), k | 1);
+		if (k | 1) < self.0.len() && self.0[k & !1] < self.0[k | 1] {
+			self.0.swap(k & !1, k | 1);
 			k ^= 1;
 		}
 		let mut p = Self::parent(k);
+		// max heap
 		while root < k && self.0[p] < self.0[k] {
 			self.0.swap(k, p);
 			k = p;
 			p = Self::parent(k)
 		}
+		// min heap
 		p |= 1;
 		while root < k && self.0[k] < self.0[p] {
 			self.0.swap(k, p);
 			k = p;
 			p = Self::parent(k) | 1;
 		}
+	}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn new_i32() {
+		let mut hq = DoublePriorityHeap::<i32>::new();
+		for i in 0..6 {
+			hq.push(&i);
+			println!("{:?}", &hq.0);
+		}
+        assert_eq!(hq.get_min(), Some(&0));
+        assert_eq!(hq.get_max(), Some(&5));
+        assert_eq!(hq.pop_min(), Some(0));
+		assert_eq!(hq.pop_min(), Some(1));
+        assert_eq!(hq.get_min(), Some(&2));
+		for i in 0..6 {
+			hq.push(&i);
+		}
+		assert_eq!(hq.pop_max(), Some(5));
+		assert_eq!(hq.get_max(), Some(&5));
+        assert_eq!(hq.pop_min(), Some(0));
+	}
+
+    #[test]
+	fn build_u64() {
+		let v = (0..8).collect::<Vec<u64>>();
+		let mut hq = DoublePriorityHeap::<u64>::from(&v);
+		println!("{:?}", &hq.0);
+        assert_eq!(hq.get_min(), Some(&0));
+        assert_eq!(hq.get_max(), Some(&7));
+        assert_eq!(hq.pop_min(), Some(0));
+		assert_eq!(hq.pop_min(), Some(1));
+        assert_eq!(hq.get_min(), Some(&2));
+		for i in 0..6 {
+			hq.push(&i);
+		}
+		assert_eq!(hq.pop_max(), Some(7));
+		assert_eq!(hq.get_max(), Some(&6));
+        assert_eq!(hq.pop_min(), Some(0));
 	}
 }
