f5655eae1a5de3a4ab8fc1278960ec943a995f1b
Whitedwarf137 <58183409+Whitedwarf137@users.noreply.github.com>
2020-11-01 16:21:41 +0900

    new, verified

---
+++ src/algorithms/tree_dfs.rs
@@ -0,0 +1,28 @@
+#![allow(dead_code)]
+use crate::utils::graph::Graph;
+
+fn tree_dfs<T: Graph>(g: &T, root: usize) -> (Vec<i64>, Vec<Option<usize>>) {
+    let n = g.size();
+	let mut dist = vec![std::i64::MAX; n];
+	dist[root] = 0;
+    let mut par = vec![None; n];
+    let mut q = vec![root];
+    while let Some(v) = q.pop() {
+        for e in g.edges_from(v) {
+            if par[v] == Some(e.to) { continue; }
+            par[e.to] = Some(v);
+            dist[e.to] = dist[v] + e.cost;
+            q.push(e.to);
+        }
+    }
+    (dist, par)
+}
+
+#[cfg(test)]
+mod tests {
+    // TODO: make tests
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
