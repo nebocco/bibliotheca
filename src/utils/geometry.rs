b50b8bf6812db4c13f146841092538bb3fbf85e5
nebocco <laserbeam299792458@gmail.com>
2020-11-11 10:50:20 +0900

    circle

--- src/utils/geometry.rs
+++ src/utils/geometry.rs
@@ -8,7 +8,7 @@ impl Point {
         Self { x, y }
     }
 
-    pub fn dist(&self, rhs: &Self) -> i64 {
+    pub fn dist2(&self, rhs: &Self) -> i64 {
         (self.x - rhs.x).pow(2) + (self.y - rhs.y).pow(2)
     }
 
@@ -18,5 +18,23 @@ impl Point {
 
     pub fn cross(&self, p: &Self, q: &Self) -> i64 {
         (p.x - self.x) * (q.y - self.y) - (p.y - self.y) * (q.x - self.x)
+    }
+}
+
+pub struct Circle {
+    pub center: Point,
+    pub radius: i64,
+}
+
+impl Circle {
+    pub fn new(x: i64, y: i64, radius: i64) -> Self {
+        Self {
+            center: Point::new(x, y),
+            radius
+        }
+    }
+    #[allow(unused_variables)]
+    pub fn intersection(&self, rhs: &Self) -> (Option<(f64, f64)>, Option<(f64, f64)>) {
+        todo!()
     }
 }
